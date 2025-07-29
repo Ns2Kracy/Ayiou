88f30a53852f2addbe34aaf7494050c2f427eaea
Ns2Kracy <ns2kracy@gmail.com>
2025-07-29 16:06:59 +0800

    update

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -1,16 +1,40 @@
 #![allow(clippy::missing_errors_doc)]
 #![allow(clippy::unnecessary_struct_initialization)]
 #![allow(clippy::unused_async)]
-use loco_rs::prelude::*;
 use axum::debug_handler;
+use loco_rs::prelude::*;
 
 #[debug_handler]
-pub async fn index(State(_ctx): State<AppContext>) -> Result<Response> {
+pub async fn login(State(_ctx): State<AppContext>) -> Result<Response> {
+    format::empty()
+}
+
+#[debug_handler]
+pub async fn register(State(_ctx): State<AppContext>) -> Result<Response> {
+    format::empty()
+}
+
+#[debug_handler]
+pub async fn current(State(_ctx): State<AppContext>) -> Result<Response> {
+    format::empty()
+}
+
+#[debug_handler]
+pub async fn forgot(State(_ctx): State<AppContext>) -> Result<Response> {
+    format::empty()
+}
+
+#[debug_handler]
+pub async fn reset(State(_ctx): State<AppContext>) -> Result<Response> {
     format::empty()
 }
 
 pub fn routes() -> Routes {
     Routes::new()
         .prefix("api/auths/")
-        .add("/", get(index))
+        .add("/login", post(login))
+        .add("/register", post(register))
+        .add("/current", get(current))
+        .add("/forgot", post(forgot))
+        .add("/reset", post(reset))
 }
